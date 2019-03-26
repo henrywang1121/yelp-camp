266ff4ef385e76aa90cbda5113b960a2fcbda9f8
Henry Wang <henry.wang@live.com>
2019-03-26 20:45:11 +0000

    minor Bootstrap decoration

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs')
+
+var campgrounds = [
+    {name: 'Walnut Creek', image: 'https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__480.jpg'}, 
+    {name: 'Mount Diablo', image: 'https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402__480.jpg'},
+    {name: 'Pleasant Hill', image: 'https://cdn.pixabay.com/photo/2016/11/21/15/14/camping-1845906__480.jpg'},
+    {name: 'Walnut Creek', image: 'https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__480.jpg'}, 
+    {name: 'Mount Diablo', image: 'https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402__480.jpg'},
+    {name: 'Pleasant Hill', image: 'https://cdn.pixabay.com/photo/2016/11/21/15/14/camping-1845906__480.jpg'},
+    {name: 'Walnut Creek', image: 'https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__480.jpg'}, 
+    {name: 'Mount Diablo', image: 'https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402__480.jpg'},
+    {name: 'Pleasant Hill', image: 'https://cdn.pixabay.com/photo/2016/11/21/15/14/camping-1845906__480.jpg'}
+    ];
+
 
 app.get('/', function(req, res){
     res.render('landing');
 })
 
 app.get('/campgrounds', function(req, res){
-    let campgrounds = [
-        {name: 'Walnut Creek', image: 'https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104490f2c870a7e5b7be_340.jpg'}, 
-        {name: 'Mount Diablo', image: 'https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104490f2c870a7e5b7be_340.jpg'},
-        {name: 'Pleasant Hill', image: 'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104490f2c870a7e5b7be_340.jpg'}];
 
     res.render('campgrounds', {campgrounds: campgrounds});
 });
+
+app.post('/campgrounds', function(req, res){
+    var name = req.body.name;
+    var image =  req.body.image;
+    var  newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect('/campgrounds')
+});
+
+app.get('/campgrounds/new', function(req, res){
+    res.render('new');
+})
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server has started.');
