bdcfa6aa05b504e98ad33ef82ef8e6e830af3eb6
Henry Wang <henry.wang@live.com>
2019-04-08 12:51:48 +0000

    destroy and edit posts

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser          = require('body-parser');
 var mongoose            = require('mongoose');
 var passport            = require('passport');
 var LocalStrategy       = require('passport-local');
+var methodOverride      = require('method-override');
 var Campground          = require('./models/campground');
 var User                = require('./models/user');
 var seedDB              = require('./seeds');
@@ -19,6 +20,7 @@ mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 
 //Add data into the database
 //seedDB();
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -51,6 +51,44 @@ router.get('/:id', function(req,res){
     });
 });
 
+//Edit Campground Route
+router.get('/:id/edit', function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            res.redirect('/campgrounds');
+        } else {
+            res.render('campgrounds/edit', {campground: foundCampground}); 
+        }
+    })
+});
+
+//Update Campground Route
+router.put('/:id',  function(req, res){
+   //find and update the correct campground
+   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+       if(err){
+           res.redirect('/campgrounds');
+       } else{
+           res.redirect('/campgrounds/' + req.params.id);
+       }
+   });
+   
+   //redirect somewhere (show page)
+});
+
+//Destroy Campground Route
+router.delete('/:id', function(req, res){
+   Campground.findByIdAndRemove(req.params.id, function(err){
+      if(err){
+          console.log(err);
+          res.redirect('/campgrounds');
+      } else {
+          res.redirect('/campgrounds');
+      }
+   });
+});
+
+
 //Middleware
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
